ff110335eee9207d0a75389bfedcac7c3bcc5cd8
Charlie Jackson <contact@charliejackson.com>
2016-11-21 13:16:36 +0000

    Added OS X support

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ if (!params.length) {
 }
 
 params.forEach(function(param) {
-  var cmd = 'start cmd /k ' + param
+  var cmd
+
+  if (process.platform === 'win32') {
+    cmd = 'start cmd /k ' + param
+  } else {
+    cmd = 'osascript -e \'tell application "Terminal" to do script "' + param + '"\''
+  }
+
+  console.log(cmd) 
+
   exec(cmd, function(error, stdout, stderr) {
     console.log(error, stdout, stderr)
   })
